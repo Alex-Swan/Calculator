c584f04bedd4d7dca34a3fb2a08dde0282c24740
Alex Swan <alex.swan1991@gmail.com>
2021-04-11 19:59:46 +0100

    Added functionality to buttons on html page, lots of repeated code that needs to be cleaned up.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let display = document.getElementById("display");
+
 const btn1 = document.getElementById("btn1");
 const btn2 = document.getElementById("btn2");
 const btn3 = document.getElementById("btn3");
@@ -9,14 +10,20 @@ const btn7 = document.getElementById("btn7");
 const btn8 = document.getElementById("btn8");
 const btn9 = document.getElementById("btn9");
 const btn0 = document.getElementById("btn0");
+
 const btnAdd = document.getElementById("btnAdd");
 const btnSubtract = document.getElementById("btnSubtract");
 const btnMultiply = document.getElementById("btnMultiply");
 const btnDivide = document.getElementById("btnDivide");
 const btnEquals = document.getElementById("btnEquals");
 const btnClear = document.getElementById("btnClear");
+
 let displayMessage = "";
-let currentNum;
+let currentNum = "";
+let num1;
+let num2;
+let operatorSymbol;
+let answer;
 
 // Simple add, subtract, multiply and divide sum function's.
 function add(a, b) {
@@ -48,4 +55,119 @@ function calcDisplay() {
   display.placeholder += displayMessage;
 }
 
-calcDisplay();
+btn1.addEventListener("click", () => {
+  currentNum += "1";
+  displayMessage = "";
+  displayMessage += "1";
+  calcDisplay();
+});
+
+btn2.addEventListener("click", () => {
+  currentNum += `2`;
+  displayMessage = "";
+  displayMessage += `2`;
+  calcDisplay();
+});
+
+btn3.addEventListener("click", () => {
+  currentNum += `3`;
+  displayMessage = "";
+  displayMessage += `3`;
+  calcDisplay();
+});
+
+btn4.addEventListener("click", () => {
+  currentNum += `4`;
+  displayMessage = "";
+  displayMessage += `4`;
+  calcDisplay();
+});
+
+btn5.addEventListener("click", () => {
+  currentNum += `5`;
+  displayMessage = "";
+  displayMessage += `5`;
+  calcDisplay();
+});
+
+btn6.addEventListener("click", () => {
+  currentNum += `6`;
+  displayMessage = "";
+  displayMessage += `6`;
+  calcDisplay();
+});
+
+btn7.addEventListener("click", () => {
+  currentNum += `7`;
+  displayMessage = "";
+  displayMessage += `7`;
+  calcDisplay();
+});
+
+btn8.addEventListener("click", () => {
+  currentNum += `8`;
+  displayMessage = "";
+  displayMessage += `8`;
+  calcDisplay();
+});
+
+btn9.addEventListener("click", () => {
+  currentNum += `9`;
+  displayMessage = "";
+  displayMessage += `9`;
+  calcDisplay();
+});
+
+btn0.addEventListener("click", () => {
+  currentNum += `0`;
+  displayMessage = "";
+  displayMessage += `0`;
+  calcDisplay();
+});
+
+btnAdd.addEventListener("click", () => {
+  num1 = parseInt(currentNum);
+  console.log(num1);
+  currentNum = "";
+  display.placeholder = "";
+  operatorSymbol = "+";
+  console.log(operatorSymbol);
+});
+
+btnSubtract.addEventListener("click", () => {
+  num1 = parseInt(currentNum);
+  console.log(num1);
+  currentNum = "";
+  display.placeholder = "";
+  operatorSymbol = "-";
+  console.log(operatorSymbol);
+});
+
+btnMultiply.addEventListener("click", () => {
+  num1 = parseInt(currentNum);
+  console.log(num1);
+  currentNum = "";
+  display.placeholder = "";
+  operatorSymbol = "*";
+  console.log(operatorSymbol);
+});
+
+btnDivide.addEventListener("click", () => {
+  num1 = parseInt(currentNum);
+  console.log(num1);
+  currentNum = "";
+  display.placeholder = "";
+  operatorSymbol = "/";
+  console.log(operatorSymbol);
+});
+
+btnEquals.addEventListener("click", () => {
+  num2 = parseInt(currentNum);
+  console.log(num2);
+  answer = operate(operatorSymbol, num1, num2);
+  currentNum = "";
+  display.placeholder = "";
+  displayMessage = answer;
+  calcDisplay();
+  console.log(answer);
+});
