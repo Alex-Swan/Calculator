214f885c3c0a69f5c4165e2c5e8d2f67e2160a06
Alex Swan <alex.swan1991@gmail.com>
2021-04-11 14:45:08 +0100

    Added some ID's to the Calc buttons. Added a function to display numbers on screen in JS

--- script.js
+++ script.js
@@ -1,29 +1,51 @@
-let display;
+let display = document.getElementById("display");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
+const btn0 = document.getElementById("btn0");
+const btnAdd = document.getElementById("btnAdd");
+const btnSubtract = document.getElementById("btnSubtract");
+const btnMultiply = document.getElementById("btnMultiply");
+const btnDivide = document.getElementById("btnDivide");
+const btnEquals = document.getElementById("btnEquals");
+const btnClear = document.getElementById("btnClear");
+let displayMessage = "";
+let currentNum;
 
+// Simple add, subtract, multiply and divide sum function's.
 function add(a, b) {
   return a + b;
 }
-
 function subtract(a, b) {
   return a - b;
 }
-
 function multiply(a, b) {
   return a * b;
 }
-
 function divide(a, b) {
   return a / b;
 }
 
 function operate(a, b, c) {
-  if (b == "+") {
-    return add(a, c);
-  } else if (b == "-") {
-    return subtract(a, c);
-  } else if (b == "*") {
-    return multiply(a, c);
-  } else if (b == "/") {
-    return divide(a, c);
+  if (a == "+") {
+    return add(b, c);
+  } else if (a == "-") {
+    return subtract(b, c);
+  } else if (a == "*") {
+    return multiply(b, c);
+  } else if (a == "/") {
+    return divide(b, c);
   }
 }
+
+function calcDisplay() {
+  display.placeholder += displayMessage;
+}
+
+calcDisplay();
